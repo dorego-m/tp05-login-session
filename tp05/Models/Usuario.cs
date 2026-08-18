25cb5bfa1e96bb81b14520d513d14e6390638993
dorego-m <manueldorego10@gmail.com>
2026-08-18 00:53:35 -0300

    algunas cosas mas

--- tp05/Models/Usuario.cs
+++ tp05/Models/Usuario.cs
@@ -3,14 +3,19 @@ namespace tp05.Models;
 public class Usuario
 {
     public int Id { get; set; }
+    public string Username { get; set; }
     public string Nombre { get; set; }
+    public string Apellido { get; set; }
     public string Password { get; set; }
+    public string TipoUsuario { get; set; }
 
-    public Usuario(int id, string nombre, string password)
+    public Usuario(int id, string nombre, string apellido, string password, string tipoUsuario)
     {
         Id = id;
         Nombre = nombre;
+        Apellido = apellido;
         Password = password;
+        TipoUsuario = tipoUsuario;
     } 
     //copilot me autocompleto esto
 }
