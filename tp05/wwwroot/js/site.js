bce437591f9de1946341cce92cb3404e4d1de697
MANUEL DO REGO BRICCHETTO <50155748@ort>
2026-09-24 09:02:01 -0300

    progresoJS

--- tp05/wwwroot/js/site.js
+++ tp05/wwwroot/js/site.js
@@ -1,4 +1,25 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const boton = document.getElementById("btnSubmit").value;
+const formulario = document.getElementById("form").value;
 
-// Write your JavaScript code.
+function AttemptSignUp()
+{
+var username = document.getElementById("username").value;
+var nombre = document.getElementById("nombre").value;
+var apellido = document.getElementById("apellido").value;
+var contraseña = document.getElementById("contraseña").value;
+
+//esto lo consegui de https://stackoverflow.com/questions/32311081/check-for-special-characters-in-string
+//estaba deprecated entonces busque como arreglarlo (con IA)
+
+var format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+//            ^                                       ^   
+var textToTest = nombre
+console.log(format.test(textToTest));
+
+
+//LO QUE FALTA:
+
+// chequea por cada error de nombre (if(!console.log(blahblahblah)))
+//despues setea un label (sin texto originalmente) a el texto especifico del error (sumar texto y un /br si hay mas de un error)
+
+}
