2a802e0920cd7c40224d8367db661d7fe28076a3
MANUEL DO REGO BRICCHETTO <50155748@ort>
2026-09-24 14:45:30 -0300

    terminadoAhoraSi

--- tp05/obj/Debug/net9.0/tp05.AssemblyInfo.cs
+++ tp05/obj/Debug/net9.0/tp05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tp05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b203079590af694785295994617c179d4ba1bcbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8973b2919baa8fa66189aa116112c30a19b72d36")]
 [assembly: System.Reflection.AssemblyProductAttribute("tp05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tp05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tp05/wwwroot/js/site.js
+++ tp05/wwwroot/js/site.js
@@ -1,25 +1,70 @@
-﻿const boton = document.getElementById("btnSubmit").value;
-const formulario = document.getElementById("form").value;
+﻿
 
 function AttemptSignUp()
 {
+var formulario = document.getElementById("form");
+
 var username = document.getElementById("username").value;
 var nombre = document.getElementById("nombre").value;
 var apellido = document.getElementById("apellido").value;
 var contraseña = document.getElementById("contraseña").value;
 
+//NOMBRE CHECK
+
+check1 = SPCharacterCheck(nombre, "Nombre", "nombreLabel");
+
+//APELLIDO CHECK
+
+check2 = SPCharacterCheck(apellido, "Apellido", "apellidoLabel");
+
+
+//USERNAME CHECK
+//la logica de estos es que si esta bien todo, no muestra nada, si hay menos que 8 characteres, siempre muestra esa alerta, y si no entonces muestra la de characteres especiales
+
+check3 = SPCharacterCheck(username, "Username", "usernameLabel");
+check4 = LengthCheck(username, "Username", "usernameLabel");
+
+//PASSWORD CHECK
+
+check5 = LengthCheck(contraseña, "Contraseña", "passwordLabel");
+
+if(check1 == "todoBien" && check2 == "todoBien" && check3 == "todoBien" && check4 == "todoBien" && check5 == "todoBien")
+{
+    formulario.requestSubmit()
+}
+
+}
+
+function SPCharacterCheck(textToTest, P, Label) 
+{
 //esto lo consegui de https://stackoverflow.com/questions/32311081/check-for-special-characters-in-string
 //estaba deprecated entonces busque como arreglarlo (con IA)
 
-var format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-//            ^                                       ^   
-var textToTest = nombre
-console.log(format.test(textToTest));
+    var format = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    //            ^                                       ^   
+    var check = "todoBien";
 
+if (format.test(textToTest)) 
+    {
+        document.getElementById(Label).textContent = P + " no puede tener characteres especiales";
+        check = "MAL";
+    }
+else 
+    {
+        document.getElementById(Label).textContent = "";
+    }
 
-//LO QUE FALTA:
+    return check;
+}
 
-// chequea por cada error de nombre (if(!console.log(blahblahblah)))
-//despues setea un label (sin texto originalmente) a el texto especifico del error (sumar texto y un /br si hay mas de un error)
+function LengthCheck(textToTest, P, Label) 
+{
+    var check = "todoBien";
+    if (textToTest.length < 8) 
+    {
+        document.getElementById(Label).textContent = P + " debe ser mas largo que 8 caracteres";
+        check = "MAL";
+    }
 
+    return check;
 }
